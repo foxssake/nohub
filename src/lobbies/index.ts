c4741bce011b12bda05bdf69c029530b9e656f58
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-05 11:34:46 +0200

    feat: Query lobby

--- src/lobbies/index.ts
+++ src/lobbies/index.ts
@@ -15,5 +15,26 @@ export const withLobbyCommands =
       const data: Map<string, string> = cmd.kvMap ?? new Map();
       const lobby = lobbyService.create(data, exchange.source.data.id);
       exchange.reply({ text: lobby.id });
+    })
+    .on("lobby/get", (cmd, exchange) => {
+      assert(cmd.isRequest, "Command must be a request!")
+      assert(cmd.text, "Missing lobby ID!")
+
+      const id = cmd.params?.at(0) ?? cmd.text
+      const properties = cmd.params 
+        ? new Set(cmd.params.slice(1))
+        : undefined
+
+      const lobby = lobbyRepository.find(id)
+      assert(lobby, `Unknown lobby #${id}!`)
+
+      const params = [id, lobby.isLocked ? 'locked' : '', !lobby.isVisible ? 'hidden' : ''].filter(it => !!it)
+
+      exchange.stream({ params })
+      for (const [key, value] of lobby.data.entries())
+        if (properties?.has(key) !== false)
+        // TODO(trimsock): Serialize kv-pairs
+          exchange.stream({ kvParams: [[key, value]], chunks: [{ text: key, isQuoted: true}, {text: "=", isQuoted: false}, {text: value, isQuoted: true}] })
+      exchange.finishStream()
     });
   };
