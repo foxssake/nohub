a618bdad0cf3c06093b7c6a9d9bbdea4b3bbbeaa
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-05 11:38:30 +0200

    comments

--- src/lobbies/index.ts
+++ src/lobbies/index.ts
@@ -18,23 +18,30 @@ export const withLobbyCommands =
         exchange.reply({ text: lobby.id });
       })
       .on("lobby/get", (cmd, exchange) => {
+        // Validate request
         assert(cmd.isRequest, "Command must be a request!");
         assert(cmd.text, "Missing lobby ID!");
 
+        // Lobby ID is either first param, or command data if no params
         const id = cmd.params?.at(0) ?? cmd.text;
+
+        // Queried properties, or undefined
         const properties = cmd.params
           ? new Set(cmd.params.slice(1))
           : undefined;
 
+        // Find lobby
         const lobby = lobbyRepository.find(id);
         assert(lobby, `Unknown lobby #${id}!`);
 
+        // Serialize keywords for first message
         const params = [
           id,
           lobby.isLocked ? "locked" : "",
           !lobby.isVisible ? "hidden" : "",
         ].filter((it) => !!it);
 
+        // Stream properties
         exchange.stream({ params });
         for (const [key, value] of lobby.data.entries())
           if (properties?.has(key) !== false)
