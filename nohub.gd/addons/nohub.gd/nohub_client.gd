c8e37abaa5437c1bacfe329ed39c9543dcbfea29
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-12 20:54:11 +0200

    feat(gd): Create lobby

--- nohub.gd/addons/nohub.gd/nohub_client.gd
+++ nohub.gd/addons/nohub.gd/nohub_client.gd
@@ -2,15 +2,20 @@ extends RefCounted
 class_name NohubClient
 
 
-var _connection: StreamPeerTCP
-var _reactor: TrimsockTCPClientReactor
+var _connection: Variant
+var _reactor: TrimsockReactor
 
 
-func _init(connection: StreamPeerTCP):
+static func from_tcp(connection: StreamPeerTCP) -> NohubClient:
+	var reactor := TrimsockTCPClientReactor.new(connection)
+	connection.set_no_delay(true)
+
+	return NohubClient.new(connection, reactor)
+
+
+func _init(connection: Variant, reactor: TrimsockReactor):
 	_connection = connection
-	_connection.set_no_delay(true)
-
-	_reactor = TrimsockTCPClientReactor.new(connection)
+	_reactor = reactor
 
 func poll() -> void:
 	_reactor.poll()
@@ -21,35 +26,10 @@ func create_lobby(address: String, data: Dictionary) -> String:
 	for key in data:
 		request.with_kv_pairs([TrimsockCommand.pair_of(key, data[key])])
 
-	var xchg := _reactor.submit_request(request)
+	var xchg := _reactor.request(_connection, request)
 	var response := await xchg.read()
 	
 	if response.is_success():
 		return response.text
 	else:
 		return ""
-
-func list_lobbies(fields: Array[String] = []) -> Array[String]:
-	var result := [] as Array[String]
-	var request := TrimsockCommand.request("lobby/list")\
-		.with_params(fields)
-
-	var xchg := _reactor.submit_request(request)
-	while xchg.is_open():
-		var cmd := await xchg.read()
-		if not cmd.is_stream_chunk():
-			continue
-		result.append(cmd.params[0]) # TODO: Parse keywords and fields
-	
-	return result
-
-func join_lobby(id: String) -> String:
-	var request := TrimsockCommand.request("lobby/join")\
-		.with_params([id])
-
-	var xchg := _reactor.submit_request(request)
-	var response := await xchg.read()
-	
-	if response.is_success():
-		return response.text
-	return ""
