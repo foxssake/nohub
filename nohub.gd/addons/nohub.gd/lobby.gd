7d8303d9d0c95b91f12892ea5d6cca0e798edcaa
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-12 21:05:44 +0200

    feat(gd): List lobbies (#24)

---
+++ nohub.gd/addons/nohub.gd/lobby.gd
@@ -0,0 +1,7 @@
+extends RefCounted
+class_name NohubLobby
+
+var id: String = ""
+var is_visible: bool = true
+var is_locked: bool = false
+var data: Dictionary = {}
--- nohub.gd/addons/nohub.gd/nohub_client.gd
+++ nohub.gd/addons/nohub.gd/nohub_client.gd
@@ -2,34 +2,53 @@ extends RefCounted
 class_name NohubClient
 
 
-var _connection: Variant
-var _reactor: TrimsockReactor
+var _connection: StreamPeerTCP
+var _reactor: TrimsockTCPClientReactor
 
 
-static func from_tcp(connection: StreamPeerTCP) -> NohubClient:
-	var reactor := TrimsockTCPClientReactor.new(connection)
-	connection.set_no_delay(true)
-
-	return NohubClient.new(connection, reactor)
-
-
-func _init(connection: Variant, reactor: TrimsockReactor):
+func _init(connection: StreamPeerTCP):
 	_connection = connection
-	_reactor = reactor
+	_connection.set_no_delay(true)
+
+	_reactor = TrimsockTCPClientReactor.new(connection)
 
 func poll() -> void:
 	_reactor.poll()
 
-func create_lobby(address: String, data: Dictionary) -> String:
+func create_lobby(address: String, data: Dictionary) -> NohubLobby:
 	var request := TrimsockCommand.request("lobby/create")\
 		.with_params([address])
 	for key in data:
 		request.with_kv_pairs([TrimsockCommand.pair_of(key, data[key])])
 
-	var xchg := _reactor.request(_connection, request)
+	var xchg := _reactor.submit_request(request)
 	var response := await xchg.read()
 	
 	if response.is_success():
-		return response.text
+		var lobby := NohubLobby.new()
+		lobby.id = response.text
+		lobby.data = data
+		return lobby
 	else:
-		return ""
+		return null
+
+func list_lobbies(fields: Array[String] = []) -> Array[NohubLobby]:
+	var result := [] as Array[NohubLobby]
+	var request := TrimsockCommand.request("lobby/list")\
+		.with_params(fields)
+
+	var xchg := _reactor.submit_request(request)
+	while xchg.is_open():
+		var cmd := await xchg.read()
+		if not cmd.is_stream_chunk():
+			continue
+
+		var lobby := NohubLobby.new()
+		lobby.id = cmd.params[0]
+		lobby.is_locked = cmd.params.has("locked")
+		lobby.is_visible = not cmd.params.has("hidden")
+		lobby.data = cmd.kv_map
+
+		result.append(lobby)
+	
+	return result
