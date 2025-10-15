e6e912ea4a5b471500a9e03d0bbd8aa8d654137b
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-15 11:43:48 +0200

    fxs

--- nohub.gd/addons/nohub.gd/lobby.gd
+++ nohub.gd/addons/nohub.gd/lobby.gd
@@ -5,3 +5,6 @@ var id: String = ""
 var is_visible: bool = true
 var is_locked: bool = false
 var data: Dictionary = {}
+
+func _to_string() -> String:
+	return "NohubLobby(id=%s, is_visible=%s, is_locked=%s, data=%s)" % [id, is_visible, is_locked, data]
