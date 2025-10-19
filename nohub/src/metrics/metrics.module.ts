1241ea6d133284b0d9f8fbc3190d4ceff2ddeba5
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-19 15:29:26 +0200

    prometheus

--- nohub/src/metrics/metrics.module.ts
+++ nohub/src/metrics/metrics.module.ts
@@ -2,19 +2,24 @@ import type { MetricsConfig } from "@src/config";
 import { rootLogger } from "@src/logger";
 import type { Module } from "@src/module";
 import type { Nohub, NohubReactor } from "@src/nohub";
+import { collectDefaultMetrics, Registry } from "prom-client";
 
 export class MetricsModule implements Module {
   private readonly logger = rootLogger.child({ name: "mod:metrics" })
+  public readonly metricsRegistry: Registry = new Registry();
 
   constructor (
     private readonly config: MetricsConfig
-  ) {}
+  ) {
+  }
 
   attachTo(app: Nohub) {
     if (!this.config.enabled) {
       this.logger.info("Metrics disabled, doing nothing.")
       return
     }
+
+    collectDefaultMetrics({ register: this.metricsRegistry });
   }
 
   configure(reactor: NohubReactor) {
@@ -27,16 +32,18 @@ export class MetricsModule implements Module {
   }
 
   private serve(hostname: string, port: number) {
+    const registry = this.metricsRegistry
+
     Bun.serve({
       hostname,
       port,
-      fetch(request) {
+      async fetch(request) {
         const url = new URL(request.url);
 
         if (request.method != "GET" || url.pathname != "/metrics")
           return new Response(undefined, { status: 404 })
 
-        return new Response(`${request.method} ${url.pathname}`)
+        return new Response(await registry.metrics())
       },
       
     }).unref();
