14709eec4215c074c293efb97d083cfdb34a34fb
Tamás Gálffy <ezittgtx@gmail.com>
2025-11-24 21:55:16 +0100

    docs(gd): Add documentation comments (#71)
    Refs #56

--- nohub.gd/addons/nohub.gd/lobby.gd
+++ nohub.gd/addons/nohub.gd/lobby.gd
@@ -1,10 +1,29 @@
 extends RefCounted
 class_name NohubLobby
 
+## Describes a lobby
+##
+## @tutorial(Lobbies): https://foxssake.github.io/nohub/understanding-nohub/concepts.html#lobbies
+
+
+## The lobby's ID - an arbitrary string unique to it
 var id: String = ""
+
+## True if the lobby is visible
+## [br][br]
+## If this is false, the lobby won't appear when listing lobbies.
 var is_visible: bool = true
+
+## True if the lobby is locked
+## [br][br]
+## When a lobby is locked, other player's won't be able to join.
 var is_locked: bool = false
+
+## Custom data associated with the lobby
+## [br][br]
+## Note that all keys and values must be strings!
 var data: Dictionary = {}
+
 
 func _to_string() -> String:
 	return "NohubLobby(id=%s, is_visible=%s, is_locked=%s, data=%s)" % [id, is_visible, is_locked, data]
--- nohub.gd/addons/nohub.gd/nohub_client.gd
+++ nohub.gd/addons/nohub.gd/nohub_client.gd
@@ -1,26 +1,59 @@
 extends RefCounted
 class_name NohubClient
 
+## Nohub client implementation
+##
+## This class provides access to all the functionality implemented in nohub. 
+## This is done via a TCP connection. To use this client, establish a connection
+## to the desired nohub server using [StreamPeerTCP], and instantiate the
+## client.
+## [br][br]
+## Make sure to regularly poll the client using [method poll]. Otherwise, client
+## calls will never return.
+## [br][br]
+## Every operation returns a [NohubResult]. If the operation is successful, the
+## result object contains the data returned by nohub. Otherwise, the result will
+## contain the error. This results in calls like this:
+## [codeblock]
+## var result := await nohub_client.list_lobbies()
+## if result.is_success():
+##     var lobbies := result.value()
+##     # ...
+## else:
+##     push_error(result.error())
+## [/codeblock]
+##
+## @tutorial(Getting started): https://foxssake.github.io/nohub/getting-started/using-nohub.html#with-godot
+## @tutorial(Understanding nohub): https://foxssake.github.io/nohub/understanding-nohub/index.html
+
 
 var _connection: StreamPeerTCP
 var _reactor: TrimsockTCPClientReactor
 
 
+## Construct a client using the specified [param connection]
 func _init(connection: StreamPeerTCP):
 	_connection = connection
 	_connection.set_no_delay(true)
 
 	_reactor = TrimsockTCPClientReactor.new(connection)
 
+## Poll the client
+## [br][br]
+## This will poll the underlying connection and process any incoming commands.
 func poll() -> void:
 	_reactor.poll()
 
+## Specify the game ID used by this client
+## [br][br]
+## See [url=https://foxssake.github.io/nohub/understanding-nohub/concepts.html#games]Games[/url].
 func set_game(id: String) -> NohubResult:
 	var request := TrimsockCommand.request("session/set-game")\
 		.with_params([id])
 	return await _bool_request(request)
 
-func create_lobby(address: String, data: Dictionary) -> NohubResult.Lobby:
+## Create a lobby
+func create_lobby(address: String, data: Dictionary = {}) -> NohubResult.Lobby:
 	var request := TrimsockCommand.request("lobby/create")\
 		.with_params([address])
 	for key in data:
@@ -34,6 +67,10 @@ func create_lobby(address: String, data: Dictionary) -> NohubResult.Lobby:
 	else:
 		return _command_to_error(response)
 
+## Query a lobby by ID
+## [br][br]
+## If [param properties] is specified, only the listed properties will be
+## returned from the lobby's custom data.
 func get_lobby(id: String, properties: Array[String] = []) -> NohubResult.Lobby:
 	var request := TrimsockCommand.request("lobby/get")\
 		.with_params([id] + properties)
@@ -45,10 +82,14 @@ func get_lobby(id: String, properties: Array[String] = []) -> NohubResult.Lobby:
 	else:
 		return _command_to_error(response)
 
-func list_lobbies(fields: Array[String] = []) -> NohubResult.LobbyList:
+## List lobbies by ID
+## [br][br]
+## If [param properties] is specified, only the listed properties will be
+## returned from the lobby's custom data.
+func list_lobbies(properties: Array[String] = []) -> NohubResult.LobbyList:
 	var result := [] as Array[NohubLobby]
 	var request := TrimsockCommand.request("lobby/list")\
-		.with_params(fields)
+		.with_params(properties)
 
 	var xchg := _reactor.submit_request(request)
 	while xchg.is_open():
@@ -63,11 +104,18 @@ func list_lobbies(fields: Array[String] = []) -> NohubResult.LobbyList:
 	
 	return NohubResult.LobbyList.of_value(result)
 
+## Delete a lobby using its ID
+## [br][br]
+## Only the lobby's owner can delete the lobby.
 func delete_lobby(lobby_id: String) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/delete")\
 		.with_params([lobby_id])
 	return await _bool_request(request)
 
+## Join a lobby using its ID
+## [br][br]
+## The response will contain the lobby's address. This string can be used to
+## connect.
 func join_lobby(lobby_id: String) -> NohubResult.Address:
 	var request := TrimsockCommand.request("lobby/join")\
 		.with_params([lobby_id])
@@ -80,32 +128,53 @@ func join_lobby(lobby_id: String) -> NohubResult.Address:
 	else:
 		return _command_to_error(response)
 
+## Lock the lobby, preventing others from joining
+## [br][br]
+## Only the lobby's owner can lock the lobby.
 func lock_lobby(lobby_id: String) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/lock")\
 		.with_params([lobby_id])
 	return await _bool_request(request)
 
+## Unlock the lobby, allowing others to join
+## [br][br]
+## Only the lobby's owner can unlock the lobby. Lobbies are unlocked by default.
 func unlock_lobby(lobby_id: String) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/unlock")\
 		.with_params([lobby_id])
 	return await _bool_request(request)
 
+## Hide lobby, making it invisible when listing lobbies
+## [br][br]
+## Only the lobby's owner can hide the lobby.
 func hide_lobby(lobby_id: String) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/hide")\
 		.with_params([lobby_id])
 	return await _bool_request(request)
 
+## Pulbish lobby, making it visible when listing lobbies
+## [br][br]
+## Only the lobby's owner can hide the lobby. Lobbies are visible by default.
 func publish_lobby(lobby_id: String) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/publish")\
 		.with_params([lobby_id])
 	return await _bool_request(request)
 
+## Set the lobby's custom data
+## [br][br]
+## Note that this method updates the data, instead of adding to it. Only the 
+## lobby's owner can update the lobby's custom data.
 func set_lobby_data(lobby_id: String, data: Dictionary) -> NohubResult:
 	var request := TrimsockCommand.request("lobby/set-data")\
 		.with_params([lobby_id])\
 		.with_kv_map(data)
 	return await _bool_request(request)
 
+## Return the client's address, as seen by the nohub server
+## [br][br]
+## This can be used to get the client's public IP address. Note that depending
+## on its configuration, the server might not be able to return a useful address
+## - e.g. when running from Docker using a bridge network.
 func whereami() -> String:
 	var request := TrimsockCommand.request("whereami")
 	var xchg := _reactor.submit_request(request)
