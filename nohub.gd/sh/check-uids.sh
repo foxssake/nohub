2ef0376cdfb0eb0867e8a6082b87ea3a093a08a5
Tamás Gálffy <ezittgtx@gmail.com>
2025-11-25 22:27:26 +0100

    build(gd): Check UIDs

---
+++ nohub.gd/sh/check-uids.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check Godot version
+if ! godot --version | grep ^4.4; then
+  echo "Wrong Godot version!"
+  godot --version
+  exit 1;
+fi
+
+echo "::group::Import project"
+godot --headless --import .
+echo "::endgroup::"
+
+UNTRACKED_FILES="$(git ls-files --others --exclude-standard)"
+if [[ "$UNTRACKED_FILES" ]]; then
+  echo "Missing UIDs detected!"
+  echo "$UNTRACKED_FILES"
+  exit 1
+else
+  echo "All UIDs are present!"
+fi
+
